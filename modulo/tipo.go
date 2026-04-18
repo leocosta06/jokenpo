f870fdf536415267b23adfb8be609b2708b1f6fb
Leonardo Costa de Almeida <leonardocalmeida2@gmail.com>
2026-04-18 16:23:26 -0400

    meu commit

---
+++ modulo/tipo.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+ func main(){
+	//fmt.Printf("type %T - Value: %v \n", true, true)
+	//fmt.Printf("type %T - Value: %v \n",1,1)
+	//fmt.Printf("type %T - Value: %v \n","1","1")
+	fmt.Printf("type %T - Value: %v \n",1.222, 1.33)
+	fmt.Printf("type %T - Value: %v \n","2","leo")
+ }
